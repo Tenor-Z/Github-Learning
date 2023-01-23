25a9a836ef311e9b9a94b5582e66cdccddac1e68
Tyler Bifolchi <55516164+Tenor-Z@users.noreply.github.com>
2023-01-23 12:50:53 -0500

    Revert "Modified both script files"
    This reverts commit 936af346c36c27ddf0b83c15cbd0c1b5bd474e59.

--- script2.py
+++ script2.py
@@ -1,4 +1,2 @@
-#Print lines from Zen of Python
-#Everybody's bird about the heard
 print("There should be one-- and preferably only one --obvious way to do it.")
 print("Although that way may not be obvious at first unless you're Dutch.")
